34bc15b962b8021963ed34b5ad7c825c559c53da
Anum Farghani <127afarghani@frhsd.com>
2026-06-04 13:57:35 -0400

    imgs

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// make
+// model
+// year
+// category   (example values - "Sports Car", "Truck", "SUV" )
+// color
+// price
+// photo
+// fuelType  (example values - "Gasoline", "Electric", "Hybrid" )
+// horsepower
+// seatingCapacity
+
