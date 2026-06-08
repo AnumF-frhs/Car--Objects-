0e4ee59f439f1719106fbf3ad5cdb8675a1838cf
Anum Farghani <127afarghani@frhsd.com>
2026-06-08 12:19:07 -0400

    vd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+// alert ('hi');
+
+let carDisplay = {
+
+    sports: {
+        make: "BMW",
+        model: "Mustang",
+        year: 2015,
+        category: "Sports Car",
+        color: "Metallic Blue",
+        price: "$63,425",
+        fuelType: "Hybrid",
+        horsepower: "Fast",
+        seatingCapacity: "Two seats"
+    },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //below are the requirements.
 // make
@@ -11,3 +44,80 @@
 // horsepower
 // seatingCapacity
 
+// i'm using examples from my object classwork to give me a reference point:
+
+// let characters = {
+
+// panther: {
+//     name: "Ann Takamaki",
+//     // age: 17,
+//     health: 284,
+//     attack: 58,
+//     defense: 34,
+
+
+// },
+
+// joker: {
+//     name: "Ren Amamiya",
+//     // age: 17,
+//     health: 999,
+//     attack: 65,
+//     defense: 45,
+    
+// },
+//  describeJ: function () {
+//     return `${this.joker.name} has an attack power of ${this.joker.attack} and can absorb damage with his ${this.joker.health} HP and ${this.joker.defense} defense`
+//     //  return `${this.panther.name} has an attack power of ${this.panther.attack} and can absorb damage with their ${this.panther.health} HP and ${this.panther.defense} defense`
+//     // name: this.name;
+//     // health: this.health;
+//     // atack: this.attack;
+
+//  },
+
+//   describeA: function () {
+//     return `${this.panther.name} has an attack power of ${this.panther.attack} and can absorb damage with her ${this.panther.health} HP and ${this.panther.defense} defense`
+//     //  return `${this.panther.name} has an attack power of ${this.panther.attack} and can absorb damage with their ${this.panther.health} HP and ${this.panther.defense} defense`
+//     // name: this.name;
+//     // health: this.health;
+//     // atack: this.attack;
+
+//  },
+
+// };
+
+// console.log(characters.describeJ())
+// console.log(characters.describeA())
+
+
+// //link html to js
+
+// // let name = document.getElementById("");
+// // let health = document.getElementById("health");
+// // let attack = document.getElementById("attack");
+
+// document.getElementById('joker-btn').addEventListener('click', showJoker);
+
+// function showJoker() {
+//  document.getElementById('names').textContent = characters.joker.name;
+//  document.getElementById('health').textContent = characters.joker.health;
+//  document.getElementById('attack').textContent = characters.joker.attack;
+//  document.getElementById('defense').textContent = characters.joker.defense;
+//  document.getElementById('description').textContent = characters.describeJ();
+
+
+
+//  //calling function
+// }
+
+//     document.getElementById('panther-btn').addEventListener('click', showPanther);
+
+// function showPanther() {
+//  document.getElementById('names').textContent = characters.panther.name;
+//  document.getElementById('health').textContent = characters.panther.health;
+//  document.getElementById('attack').textContent = characters.panther.attack;
+//  document.getElementById('defense').textContent = characters.panther.defense;
+//  document.getElementById('description').textContent = characters.describeA();
+
+
+//     }
